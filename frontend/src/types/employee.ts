e5138a9882c2ee7847d35c21388e755ddf1ea8bb
polakit222-lang <polakit222@gmail.com>
2025-09-12 15:50:02 +0700

    fix_type

--- frontend/src/types/employee.ts
+++ frontend/src/types/employee.ts
@@ -1,4 +1,4 @@
-import type dayjs from "dayjs";
+//import type dayjs from "dayjs";
 
 export type Sex = ""|"male" | "female" | "other";
 export type JobType = ""|"Full-time" | "Part-time" | "Contract";
@@ -12,7 +12,7 @@ export interface Employee {
   phone: string;
   address: string;
   sex: Sex;
-  birthday: dayjs.Dayjs;
+  birthday: string;
   position: string;
   jobType: JobType;
   totalSales: string;
